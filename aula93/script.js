1c9454699eba7741bba21b63534d9c0596547151
lucasfernande <lucas.fernandes9415@outlook.com>
2023-08-15 13:29:22 -0300

    nomeando importações

--- aula93/script.js
+++ aula93/script.js
@@ -1,7 +1,7 @@
-import { soma, subtrair } from './math.js'
+import * as math from './math.js'
 import multiplicar from './math.js'
 // por ser o membro padrão, não precisa das chaves
 
-console.log(soma(5, 12))
-console.log(subtrair(9, 2))
-console.log(multiplicar(8, 6))
+console.log(math.soma(5, 12))
+console.log(math.subtrair(9, 2))
+console.log(math.default(8, 6))
