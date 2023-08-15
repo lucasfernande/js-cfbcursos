572c1a53180cf98bd131b2f64381f4c61053038a
lucasfernande <lucas.fernandes9415@outlook.com>
2023-08-15 11:38:40 -0300

    entendendo módulos

---
+++ aula93/math.js
@@ -0,0 +1,9 @@
+function soma(a, b) {
+    return a + b
+}
+
+function subtrair(a, b) {
+    return a - b
+}
+
+export {soma, subtrair}
---
+++ aula93/script.js
@@ -0,0 +1,4 @@
+import { soma, subtrair } from './math.js'
+
+console.log(soma(5, 12))
+console.log(subtrair(9, 2));
