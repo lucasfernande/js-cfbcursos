2c94e919849738b03abe77bd56d1cfebc81d671b
lucasfernande <lucas.fernandes9415@outlook.com>
2023-08-20 14:12:11 -0300

    importando classes

---
+++ aula96/script.js
@@ -0,0 +1,8 @@
+import Operacoes from "./teste.js";
+// import * as op from './teste.js'
+
+// console.log(op.default.somar(4, 5));
+// console.log(op.default.mult(8, 2));
+
+console.log(Operacoes.div(30, 5));
+console.log(Operacoes.subt(22, 3));
---
+++ aula96/teste.js
@@ -0,0 +1,19 @@
+class Operacoes {
+    static somar(a, b) {
+        return a + b
+    }
+
+    static subt(a, b) {
+        return a - b
+    }
+
+    static mult(a, b) {
+        return a * b
+    }
+
+    static div(a, b) {
+        return a / b
+    }
+}
+
+export default Operacoes
